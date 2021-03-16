bc9c0b145e6ada5f3a2c1dfd76aebfce449bd7ec
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-03-15 23:52:48 -0500

    se agregan funciones para leer documento .md

--- src/main.mjs
+++ src/main.mjs
@@ -1,38 +1,44 @@
 /* ESM script example */
-import { fstat } from 'fs';
+import { fstat, lstat, ReadStream, Stats } from 'fs';
 import { dirname, isAbsolute, resolve, parse } from 'path';
 import { fileURLToPath, pathToFileURL } from 'url';
-import { mdLinks, read } from './util.mjs';
+import { mdLinks, read} from './util.mjs';
 
 
-/* const dirnameFunction = dirname(fileURLToPath(import.meta.url)); */
+const dirnameFunction = dirname(fileURLToPath(import.meta.url));
 //funcion recibe path
-/* console.log(dirnameFunction) */
-const ruta = '../src'
+console.log(dirnameFunction)
+const ruta = /* dirnameFunction + '/archivo.md'; */ /* '../practica' */'../src'
 function verificarRuta(ruta){
+  console.log(ruta)
+  //isabsolute ask if not..is relative so is false
   if(isAbsolute(ruta)==false){
+    if(parse(ruta).ext ==''){
+      /* console.log(parse(ruta).ext) */
     mdLinks(resolve(ruta))
-    .then((data) => { console.log(data) })
-  }
+    .then((data) => {
+      for(const type of data){
+      if(parse(type).ext == ".md"){console.log(resolve(ruta) + `/${type}`),
+       read(resolve(ruta) + `/${type}`).then((hola) => console.log(hola.toString('utf8')))}
+      }})}
+    else if(parse(type).ext == ".md"){console.log(resolve(ruta) + `/${type}`),
+    read(resolve(ruta) + `/${type}`).then((hola) => console.log(hola.toString('utf8')))}
+
+    else{console.log("no hay nada en esta ruta")}}
+
   else{
-    mdLinks(resolve(ruta))
-    .then((data) => { console.log(data) })
-  }
-}
+    if(parse(ruta).ext ==''){
+      mdLinks(ruta)
+      .then((data) => {
+        for(const type of data){
+        if(parse(type).ext == ".md"){console.log(ruta + `/${type}`),
+         read(resolve(ruta) + `/${type}`).then((hola) => console.log(hola.toString('utf8')))}
+        else{
+          console.log("no encuentro archivos .md")
+        }}})}
+    else if(parse(ruta).ext == ".md"){console.log(ruta),
+    read(ruta).then((hola) => console.log(hola.toString('utf8')))
+      }
+    else{console.log("no hay nada en esta ruta")}
+    }};
 verificarRuta(ruta)
-for(const type of verificarRuta(ruta)){
-  console.log(`this file es ${type}`)
-}
-
-/*
-read(resolve(ruta))
-    .then((data) => console.log(data)) */
-
-/* console.log(dirnameFunction) */
-//ingresa nombre de archivo.md
-//ruta relativa
-/* mdLinks(dirnameFunction + '/archivo.md')
-  .then((data) => console.log(data.toString('utf8')))
-  ****exname
- */
-
--- src/util.mjs
+++ src/util.mjs
@@ -1,6 +1,7 @@
 /* ESM script example */
 import * as fs from 'fs';
 
+// reading folder
 const mdLinks = (path) => {
     return new Promise((resolve, reject)=>{
       fs.readdir(path,(err, data)=>{
@@ -11,8 +12,8 @@ const mdLinks = (path) => {
 const read = (path) =>{
       return new Promise((resolve, reject)=>{
         fs.readFile(path, (err, data)=>{
-          /*   if(err) throw err;
-            console.error("already")*/
+             /* if(err) throw err;
+            console.error("already") */
              resolve(data)})})}
 
  export{ mdLinks, read };
