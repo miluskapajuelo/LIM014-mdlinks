0ce567ab4d8793f0f1d58073819bb9407997fdc2
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-03-23 17:28:55 -0500

    se modularizan funciones

--- src/main.mjs
+++ src/main.mjs
@@ -1,59 +1,14 @@
 /* ESM script example */
 import { rejects } from 'assert'
-import { dirname, extname  } from 'path'
+import { dirname, extname } from 'path'
 import { fileURLToPath, pathToFileURL } from 'url'
-import {
-    readFile,
-    PathDirectory,
-    convertPath,
-    getText,
-} from './util.mjs'
-import { readdir,statSync } from 'fs'
+import { reader, Finder } from './util.mjs'
+import { readdirSync, statSync } from 'fs'
 
 /* const dirnameFunction = dirname(fileURLToPath(import.meta.url)) */
 const cwd = pathToFileURL(`${process.cwd()}/`).href
 // dirnameFunction + '/archivo.md'
-const regexLinkFull = /\[[a-zA-Z0-9-.]+\](www\.|https?:\/\/)?[a-zA-Z0-9-.]+[/a-zA-Z0-9-.]+/gim
-
-
-
-function rutas(path){
-  let array=[];
-  let ruta = convertPath(path)
-  statSync(ruta)
-    if (PathDirectory(ruta)) {
-        readdir(ruta, (err, files) => {
-            if (files.length == 0) {
-                return 'no hay información aquí'
-            }
-            else {
-                files.forEach((file) => {
-                    rutas(ruta + `/${file}`)
-                })
-            }
-        })
-    }
-
-    if (extname(ruta).toLowerCase() == '.md') {
-      let text = regexLinkFull.exec(readFile(ruta))
-      if(text !== null){
-        let href = getText(...text)
-        let Data = {'href': href[1],'text': href[0].substring(0, 50),'file': path};
-        array.push(Data)
-        return array}
-
-              }
-
-            }
 const ruta = '../practica'
-console.log(rutas(ruta))
 
+console.log(Finder(ruta))
 
-/* console.log(rutas(ruta)); */
-
-/* const mdLinks = function (ruta) {
-    let path = convertPath(ruta)
-          rutas(path)}
-
-mdLinks(ruta)
- */
--- src/util.mjs
+++ src/util.mjs
@@ -1,19 +1,67 @@
 /* ESM script example */
+//Import modules
 import { count } from 'console';
 import { text } from 'express';
-import {readdirSync, readFileSync, lstatSync, existsSync } from 'fs'
-import{isAbsolute, resolve} from 'path';
+import {readdirSync, readFileSync, statSync, existsSync } from 'fs'
+import{isAbsolute, resolve, extname, normalize} from 'path';
 
+//Regular Expressions
 const regexLink = /(www\.|https?:\/\/)?[a-zA-Z0-9-.]+[/a-zA-Z0-9-.]+/gim
+const regexLinkFull = /\[[a-zA-Z0-9-.]+\](www\.|https?:\/\/)?[a-zA-Z0-9-.]+[/a-zA-Z0-9-.]+/gim
 
-
-const convertPath = (path) => isAbsolute(path) ? path : resolve(path)
-const itExtist = (path) => existsSync(path)
-const PathDirectory = (path) => lstatSync(path).isDirectory()
+//Short functions
+const itExist = (path) => existsSync(path)
+const PathDirectory = (path) => statSync(path).isDirectory()
 const readDir = (path) => readdirSync(path)
 const readFile = (path) =>readFileSync(path).toString('utf8')
 const getText =(text) => text.match(regexLink)
 
- export{ readDir, readFile, itExtist, PathDirectory,convertPath, getText };
+//Convert path and normalize
+const convertPath = (path) =>{
+
+  let normalizePath = normalize(path);
+  let result;
+
+  if (!isAbsolute(normalizePath))   {
+    result = resolve(normalizePath);
+  }
+  result = path;
+
+  return result;
+}
+
+//Read path
+const reader =(path) => {
+  let array=[];
+  if (extname(path)== '.md') {
+  let text = regexLinkFull.exec(readFile(path))
+    if(text !== null){
+    let href = getText(...text)
+    let Data = {'href': href[1],'text': href[0].substring(0, 50),'file': path};
+    array.push(Data)
+    }
+  }
+  return array}
+
+//Find .md files
+ function Finder(path) {
+  let array2 = []
+  let ruta = convertPath(path)
+  if (PathDirectory(ruta)) {
+      const routes = readDir(ruta)
+      if (routes.length == 0) {
+          return 'no hay información aquí'
+      }
+      routes.forEach((file) => {
+          const route = Finder(ruta + `/${file}`)
+          array2 = array2.concat(ruta + `/${file}`)
+      })
+  }
+
+  return array2
+}
+
+//Export functions
+export{reader, Finder };
 
 
